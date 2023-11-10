5194705088add24ead68a1db8ac489f5257035f6
Sercan Yavan <srcnyvn@gmail.com>
2023-11-10 16:37:02 +0300

    son hali.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: const Color.fromARGB(255, 70, 4, 105), // Configuration Widgets
-      body: Center(
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor:
+            const Color.fromARGB(255, 70, 4, 105), // Configuration Widgets
+        body: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("assets/sercan.jpg", width: 300, height: 300,),
-          const Text("Sercan Yavan",
-              style: TextStyle(fontSize: 35, color: Colors.white)),
-          const Text("Mobil Flutter  -  1-A",
-              style: TextStyle(fontSize: 35, color: Colors.white)),
-          const Text(
-            "31-10-2023",
-            style: TextStyle(fontSize: 35, color: Colors.white),
-          )
-        ],
-      )),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/sercan.jpg",
+                width: 300,
+                height: 300,
+              ),
+              const Text("Sercan Yavan",
+                  style: TextStyle(fontSize: 35, color: Colors.white)),
+              const Text("Mobil Flutter  -  1-A",
+                  style: TextStyle(fontSize: 35, color: Colors.white)),
+              const Text(
+                "31-10-2023",
+                style: TextStyle(fontSize: 35, color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
 
 // intelilisense => cmd + i 
